4d18b6e72f1dc5eae7ee94f4209c35ab8a16e2a5
Julian <wjhessels@gmail.com>
2019-07-19 16:27:56 +0200

    aanpassingen gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
 
 // compile scss into css
@@ -15,6 +16,12 @@ function style() {
 
 }
 
+function miniimage() {
+	return gulp.src('./images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+}
+
 function watch() {
 	browserSync.init({
 		server: {
@@ -22,6 +29,7 @@ function watch() {
 		}
 	});
 	gulp.watch('./scss/**/*.scss', style);
+	gulp.watch('./images/*', miniimage);
 	gulp.watch('./*.html').on('change', browserSync.reload);
 	gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
